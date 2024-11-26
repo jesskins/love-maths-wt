a99e79056354a0e79b28a025593490a3b2ce1321
Jessica Howkins <jess.howkins@yahoo.com>
2024-11-26 00:26:34 +0000

    added js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,59 @@
 console.log("Connected!");
+
+// 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document-getElementsByTagName("button");
+
+    for (let buttons of buttons) {
+        button.addEventListener("click", function() {
+            if (thiss.getAttribute("data-type") === "submit") {
+                alert("You clilcked Submut");
+            } else {
+                let game-type = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+            }};
+
+            }
+        });
+
+
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+// literally the score
+
+function incrementScore() {
+
+}
+
+// num of wrong tries
+
+function incrementWrongAnswer() {
+
+}
+
+// questions:
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
